76172ee22099630fbadd3b2bb67994011a912061
Mihail Moscalu <mmoskalu@gmail.com>
2017-12-13 20:49:34 +0200

    test gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require ("gulp"); // подключение gulp
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task ("test", function () {
+    console.log ("Hello world")
+} );
+
+gulp.task ("autoprefixer", function () {
+    gulp.src('source/css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css'))
+} );
